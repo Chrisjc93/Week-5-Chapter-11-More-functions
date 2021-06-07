066fad5316ab6d78538dc1443a7f407bbcc9a020
chrisjc9931 <>
2021-06-07 03:50:45 +0000

    added exercises to end

--- index.js
+++ index.js
@@ -327,3 +327,44 @@ function decreasingSum(integer) {
 }
 
 console.log(decreasingSum(5));
+
+//Create an anonymous fucntion and set it equal to a variable.
+// let anonFunction = function(input){
+//   if(typeof(input) === "string"){
+//     return `ARRR!`;
+//   } else if (typeof(input) === "number"){
+//     return input * 3;
+//   } else {
+//     return `Invalid Input!!!`;
+//   }
+// };
+
+// console.log(anonFunction('2'));
+
+
+/* Your function should:
+a) If passed a number, return the tripled value.
+b) If passed a string, return the string “ARRR!”
+c) Be sure to test your function before moving on the next part. */
+
+
+
+/* Use your fuction and the map method to change an array  as follows:
+a) Triple any the numbers.
+b) Replace any strings with “ARRR!”
+c) Print the new array to confirm your work.
+*/
+let arr = ['Elocution', 21, 'Clean teeth', 100];
+
+
+let anonFunction = arr.map(function(input){
+  if(typeof(input) === "string"){
+    return `ARRR!`;
+  } else if (typeof(input) === "number"){
+    return input * 3;
+  } else {
+    return `Invalid Input!!!`;
+  }
+});
+
+console.log(anonFunction);
